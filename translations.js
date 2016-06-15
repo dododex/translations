79fb40ba2043e6a8f06d82cd06145d72a0c60a90
Dan Leveille <dan14lev@gmail.com>
2016-06-14 23:27:02 -0700

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,88 @@
+Taming calculator
+Taming
+Timers
+Food
+Use // Context: Use this food
+Max
+Time
+Total Time
+Taming Effectiveness
+Current lvl
+Lvl
+With taming bonus
+Max after training // Context: Max level after training
+Narcotics needed
+Knock out
+Edit Weapons
+Weapons
+Primary // Context: Primary attack
+Secondary
+Charge // Pachy charge attack
+Damage modifier chart
+Stats
+Calculator
+Details
+Comparison 
+Base // Context: Base level
+Health
+Stamina
+Oxygen
+Food
+Weight
+Melee Damage
+"Wasted" Points
+pts //points
+Favourite Kibble
+Kibble Produced
+Crafted in
+Cooking Pot
+Kibble flow chart
+Saddles
+Gathering efficiency
+Gathering efficiency ratings
+Top resources
+Full chart
+Rate this creature 
+Based on ratings from {{x}} Dododex users.
+Breeding
+Ideal incubation temperature // Context: Egg incubation
+Mating interval // Context: Interval to wait in between mating
+Incubation
+Pregnancy
+Baby
+Total maturation // Context: Total time require to mature
+Juvenile
+Adolescent
+More // Context: More info about this creature
+Carryable by // Context: This creature is carryable by a quetzal
+Carryable creatures chart
+Wiki page
+Can damage // Context: This creature can damage stone
+Affected by // Context: This creature is affected by 
+XP for killing:
+Map
+About
+Feedback
+Settings
+Timers
+Back // Context: Back button
+Multiplier
+Sever Multipliers
+Adjust multipliers for unofficial server settings. (Keep multipliers at 1 for official servers.)
+Taming
+Taming Speed
+Consumption Speed
+Breeding
+Egg Hatch Speed
+Also impacts gestation speed
+Baby Mature Speed
+Mating interval
+Character settings
+Melee damage
+Impacts melee weapons.
+App Settings
+Use text input for level selector
+Type in levels rather than using a picker.
+Advanced
+Check for new data
+Dododex automatically checks for data updates for you, but you can choose to force Dododex to download the latest data right now (app will refresh).
