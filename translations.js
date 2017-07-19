b0b354c7088f74fd618de0c2bd7c746de4195c54
Dan Leveille <dan14lev@gmail.com>
2017-07-18 23:51:23 -0700

    Creature Torpor Rank -> Creature Torpor Drop Rate

--- translations.js
+++ translations.js
@@ -330,7 +330,7 @@ var translations = {
         currentWeight: "Current Weight",
         maxWeight: "Max Weight",
         info: "Info",
-        torporClass: "Creature Torpor Rank",
+        torporClass: "Creature Torpor Drop Rate",
         extremelyHigh: "Extremely High",
         high: "High",
         low: "Low",
